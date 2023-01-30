10656f78dd095bb8697a9dbf3b57feaf33a57845
BuildTools <unconfigured@null.spigotmc.org>
2023-01-30 20:51:03 +0100

    v0.0.48 Changed some things for placement of crusher and the texture of the crusher item

--- src/main/java/mopsy/productions/nucleartech/ModBlocks/blocks/machines/CrusherBlock.java
+++ src/main/java/mopsy/productions/nucleartech/ModBlocks/blocks/machines/CrusherBlock.java
@@ -9,17 +9,20 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.ItemScatterer;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.*;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class CrusherBlock extends BlockWithEntity implements IModID, BlockEntityProvider {
+    public static final DirectionProperty FACING;
     @Override
     public String getID(){return "crusher";}
 
@@ -30,6 +33,21 @@ public class CrusherBlock extends BlockWithEntity implements IModID, BlockEntity
                 .sounds(BlockSoundGroup.METAL)
                 .requiresTool()
         );
+        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
+    }
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+    }
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
     @Override
@@ -71,4 +89,9 @@ public class CrusherBlock extends BlockWithEntity implements IModID, BlockEntity
         }
         super.onBreak(world, pos, state, player);
     }
+
+    static {
+        FACING = HorizontalFacingBlock.FACING;
+    }
+
 }
