11cfee77c2b82a6d938910f6e7cc60f3619043f7
Mopsy_14_Desktop <unconfigured@null.spigotmc.org>
2023-03-01 17:19:01 +0100

    v0.0.135 Added the code for the facing blockstates of the code.

--- src/main/java/mopsy/productions/nucleartech/ModBlocks/blocks/multiblocks/ConduitBlock.java
+++ src/main/java/mopsy/productions/nucleartech/ModBlocks/blocks/multiblocks/ConduitBlock.java
@@ -4,18 +4,22 @@ import mopsy.productions.nucleartech.interfaces.IMBBlock;
 import mopsy.productions.nucleartech.interfaces.IModID;
 import mopsy.productions.nucleartech.multiblock.MBUtils;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.MapColor;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class ConduitBlock extends Block implements IModID, IMBBlock {
+    public static final DirectionProperty FACING;
     @Override
     public String getID(){return "conduit";}
     public ConduitBlock() {
@@ -25,7 +29,28 @@ public class ConduitBlock extends Block implements IModID, IMBBlock {
                         .sounds(BlockSoundGroup.STONE)
                         .requiresTool()
         );
+        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
     }
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+    }
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    static {
+        FACING = HorizontalFacingBlock.FACING;
+    }
+
+
     @Override
     public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
         super.onStateReplaced(state, world, pos, newState, moved);
