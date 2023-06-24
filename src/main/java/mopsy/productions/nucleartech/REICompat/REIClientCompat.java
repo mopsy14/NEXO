40813f2dbb81366b2ee1d525f549a40c6462b7d8
Mopsy_14_Desktop <unconfigured@null.spigotmc.org>
2023-06-24 12:04:39 +0200

    v0.0.285 Reverted some changes to the REI Compat.

--- src/main/java/mopsy/productions/nucleartech/REICompat/REIClientCompat.java
+++ src/main/java/mopsy/productions/nucleartech/REICompat/REIClientCompat.java
@@ -1,32 +1,12 @@
 package mopsy.productions.nucleartech.REICompat;
 
-import dev.architectury.event.CompoundEventResult;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.common.fluid.FluidSupportProvider;
-import me.shedaniel.rei.api.common.util.EntryStacks;
-import mopsy.productions.nucleartech.interfaces.IBucketFluidStorage;
-import net.minecraft.fluid.Fluid;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-
-import java.util.stream.Stream;
 
 public class REIClientCompat implements REIClientPlugin {
     @Override
     public void registerFluidSupport(FluidSupportProvider support) {
-        System.out.println("Registering fluid support");
-        support.register(stack -> {
-            ItemStack itemStack = stack.getValue();
-            Item item = itemStack.getItem();
-            System.out.println("Checking item: "+item);
-            if(item instanceof IBucketFluidStorage bucketFluidStorage) {
-                Fluid fluid = bucketFluidStorage.getFluid();
-                System.out.println("Registering: "+fluid);
-                if (fluid != null)
-                    return CompoundEventResult.interruptTrue(Stream.of(EntryStacks.of(fluid)));
-                System.out.println("fluid==null");
-            }
-            return CompoundEventResult.pass();
-        });
+
     }
+
 }
--- src/main/java/mopsy/productions/nucleartech/REICompat/REICompatMain.java
+++
@@ -1,7 +0,0 @@
-package mopsy.productions.nucleartech.REICompat;
-
-public class REICompatMain {
-    public static void InitREICompat(){
-
-    }
-}
