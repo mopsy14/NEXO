b52225fa95bee66fe1bede2ee2c532df77787854
Mopsy_14_Desktop <unconfigured@null.spigotmc.org>
2023-06-26 22:54:25 +0200

    v0.0.297 Added more REI compat code.

--- src/main/java/mopsy/productions/nucleartech/REICompat/REIClientCompat.java
+++ src/main/java/mopsy/productions/nucleartech/REICompat/REIClientCompat.java
@@ -3,14 +3,20 @@ package mopsy.productions.nucleartech.REICompat;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.util.EntryStacks;
 import mopsy.productions.nucleartech.REICompat.categories.CrushingCategory;
 import mopsy.productions.nucleartech.REICompat.categories.CrushingDisplay;
 import mopsy.productions.nucleartech.recipes.CrusherRecipe;
+import mopsy.productions.nucleartech.registry.ModdedBlocks;
+
+import static mopsy.productions.nucleartech.Main.modid;
 
 public class REIClientCompat implements REIClientPlugin {
     @Override
     public void registerCategories(CategoryRegistry registry) {
         registry.add(new CrushingCategory());
+        registry.addWorkstations(CategoryIdentifier.of(modid,"crushing"), EntryStacks.of(ModdedBlocks.BlockItems.get("crusher")));
     }
 
     @Override
--- src/main/java/mopsy/productions/nucleartech/REICompat/categories/CrushingCategory.java
+++ src/main/java/mopsy/productions/nucleartech/REICompat/categories/CrushingCategory.java
@@ -1,11 +1,18 @@
 package mopsy.productions.nucleartech.REICompat.categories;
 
+import me.shedaniel.math.Point;
+import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.gui.Renderer;
+import me.shedaniel.rei.api.client.gui.widgets.Widget;
+import me.shedaniel.rei.api.client.gui.widgets.Widgets;
 import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import mopsy.productions.nucleartech.registry.ModdedBlocks;
 import net.minecraft.text.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static mopsy.productions.nucleartech.Main.modid;
 
@@ -24,5 +31,12 @@ public class CrushingCategory implements DisplayCategory<CrushingDisplay> {
         return EntryStacks.of(ModdedBlocks.BlockItems.get("crusher"));
     }
 
-
+    @Override
+    public List<Widget> setupDisplay(CrushingDisplay display, Rectangle bounds) {
+        List<Widget> widgets = new ArrayList<>();
+        widgets.add(Widgets.createRecipeBase(bounds));
+        widgets.add(Widgets.createSlot(new Point(33+ bounds.x,24+ bounds.y)).markInput().entries(display.getInputEntries().get(0)));
+        widgets.add(Widgets.createSlot(new Point(99+ bounds.x,24+ bounds.y)).markOutput().entries(display.getOutputEntries().get(0)));
+        return widgets;
+    }
 }
