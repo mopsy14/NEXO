d9335b4d58af92bc5d6ff2c52adf1ae5d1bc9e90
mopsy14 <Redstone.computerboy@gmail.com>
2023-09-25 20:46:19 +0200

    v9 Added some new code for the new cable system.

--- src/main/java/mopsy/productions/nexo/ModBlocks/blocks/InsulatedCopperCableBlock.java
+++ src/main/java/mopsy/productions/nexo/ModBlocks/blocks/InsulatedCopperCableBlock.java
@@ -3,23 +3,27 @@ package mopsy.productions.nexo.ModBlocks.blocks;
 import mopsy.productions.nexo.ModBlocks.entities.InsulatedCopperCableEntity;
 import mopsy.productions.nexo.interfaces.IModID;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.BlockWithEntity;
-import net.minecraft.block.MapColor;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.StateManager;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
+import team.reborn.energy.api.EnergyStorage;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.BiPredicate;
+
+import static net.minecraft.state.property.Properties.*;
 
 @SuppressWarnings("deprecation")
 public class InsulatedCopperCableBlock extends BlockWithEntity implements IModID {
@@ -33,6 +37,44 @@ public class InsulatedCopperCableBlock extends BlockWithEntity implements IModID
                         .sounds(BlockSoundGroup.COPPER)
                         .requiresTool()
         );
+        this.setDefaultState(this.stateManager.getDefaultState().with(UP, false).with(DOWN, false).with(NORTH, false).with(EAST, false).with(SOUTH, false).with(WEST, false));
+    }
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        BlockState res = this.getDefaultState();
+        if(isEnergyBlock(ctx.getWorld(),ctx.getBlockPos().up(),Direction.UP))
+            res.with(UP,true);
+        if(isEnergyBlock(ctx.getWorld(),ctx.getBlockPos().down(),Direction.DOWN))
+            res.with(DOWN,true);
+        if(isEnergyBlock(ctx.getWorld(),ctx.getBlockPos().north(),Direction.NORTH))
+            res.with(NORTH,true);
+        if(isEnergyBlock(ctx.getWorld(),ctx.getBlockPos().east(),Direction.EAST))
+            res.with(EAST,true);
+        if(isEnergyBlock(ctx.getWorld(),ctx.getBlockPos().south(),Direction.SOUTH))
+            res.with(SOUTH,true);
+        if(isEnergyBlock(ctx.getWorld(),ctx.getBlockPos().west(),Direction.WEST))
+            res.with(WEST,true);
+        return res;
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState originalState, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        switch (direction){
+            case UP -> {return originalState.with(UP, isEnergyBlock(world, neighborPos, Direction.UP));}
+            case DOWN -> {return originalState.with(DOWN, isEnergyBlock(world, neighborPos, Direction.DOWN));}
+            case NORTH -> {return originalState.with(NORTH, isEnergyBlock(world, neighborPos, Direction.NORTH));}
+            case EAST -> {return originalState.with(EAST, isEnergyBlock(world, neighborPos, Direction.EAST));}
+            case SOUTH -> {return originalState.with(SOUTH, isEnergyBlock(world, neighborPos, Direction.SOUTH));}
+            case WEST -> {return originalState.with(WEST, isEnergyBlock(world, neighborPos, Direction.WEST));}
+        }
+        return originalState;
+    }
+
+    private boolean isEnergyBlock(World world, BlockPos pos, Direction direction){
+        return EnergyStorage.SIDED.find(world, pos, direction) != null;
+    }
+    private boolean isEnergyBlock(WorldAccess world, BlockPos pos, Direction direction){
+        return EnergyStorage.SIDED.find((World)world, pos, direction) != null;
     }
 
     @Nullable
@@ -53,6 +95,22 @@ public class InsulatedCopperCableBlock extends BlockWithEntity implements IModID
     public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
         super.onPlaced(world, pos, state, placer, itemStack);
         if(world.isClient())return;
+
+        setCorrectState(world, pos, state);
+    }
+
+    private void setCorrectState(World world, BlockPos pos, BlockState state){
+        state.with(UP, isEnergyBlock(world, pos.up(), Direction.UP));
+        state.with(DOWN, isEnergyBlock(world, pos.down(), Direction.DOWN));
+        state.with(NORTH, isEnergyBlock(world, pos.north(), Direction.NORTH));
+        state.with(EAST, isEnergyBlock(world, pos.east(), Direction.EAST));
+        state.with(SOUTH, isEnergyBlock(world, pos.south(), Direction.SOUTH));
+        state.with(WEST, isEnergyBlock(world, pos.west(), Direction.WEST));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(UP, DOWN, NORTH, EAST, WEST, SOUTH);
     }
 
     private List<InsulatedCopperCableEntity> getConnectedCables(World world, BlockPos pos){
--- src/main/java/mopsy/productions/nexo/ModBlocks/entities/InsulatedCopperCableEntity.java
+++ src/main/java/mopsy/productions/nexo/ModBlocks/entities/InsulatedCopperCableEntity.java
@@ -5,10 +5,27 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import team.reborn.energy.api.EnergyStorage;
+import team.reborn.energy.api.base.SimpleEnergyStorage;
+
+import java.util.*;
 
 @SuppressWarnings("UnstableApiUsage")
 public class InsulatedCopperCableEntity extends BlockEntity{
+
+    public static final long POWER_CAPACITY = 10000;
+    public static final long MAX_POWER_INSERT = 10000;
+    public static final long MAX_POWER_EXTRACT = 0;
+
+    public final SimpleEnergyStorage energyStorage = new SimpleEnergyStorage(POWER_CAPACITY,MAX_POWER_INSERT,MAX_POWER_EXTRACT){
+        @Override
+        protected void onFinalCommit() {
+        }
+    };
+
+    public Set<EnergyStorage> connectedStorages = new HashSet<>(6);
 
     public InsulatedCopperCableEntity(BlockPos pos, BlockState state) {
         super(ModdedBlockEntities.INSULATED_COPPER_CABLE, pos, state);
@@ -25,5 +42,82 @@ public class InsulatedCopperCableEntity extends BlockEntity{
     public static void tick(World world, BlockPos blockPos, BlockState blockState, InsulatedCopperCableEntity entity) {
         if(world.isClient)return;
 
+        updateStorages(world,blockPos,entity);
+
+        if(entity.energyStorage.amount > 0){
+
+            List<InsulatedCopperCableEntity> cables = new ArrayList<>(entity.getConnectedCables());
+            Collections.shuffle(cables);
+
+
+        }
+    }
+    private Set<EnergyStorage> getAllOutputStorages(){
+
+        Set<EnergyStorage> res = new HashSet<>();
+
+        for(InsulatedCopperCableEntity cable : getConnectedCables()){
+            res.addAll(cable.connectedStorages);
+        }
+
+        return res;
+    }
+    private List<InsulatedCopperCableEntity> getAllConnectedCables(){
+        List<InsulatedCopperCableEntity> res = new ArrayList<>();
+        List<InsulatedCopperCableEntity> toCheck = new ArrayList<>();
+        List<BlockPos> shouldIgnore = new ArrayList<>();
+        shouldIgnore.add(this.pos);
+        toCheck.add(this);
+
+        while(!toCheck.isEmpty()){
+            getSurroundingCablePosses(toCheck.get(toCheck.size()-1).pos, shouldIgnore);
+        }
+
+        return res;
+    }
+    private List<InsulatedCopperCableEntity> getSurroundingCablePosses(BlockPos pos, List<BlockPos> shouldIgnore){
+        List<InsulatedCopperCableEntity> res = new ArrayList<>(6);
+        for(Direction direction : Direction.values()){
+             BlockPos iteratorPos = pos.add(direction.getVector());
+             if(!shouldIgnore.contains(iteratorPos))
+                if(world.getBlockEntity(iteratorPos) instanceof InsulatedCopperCableEntity cable) {
+                    res.add(cable);
+                    shouldIgnore.add(iteratorPos);
+                }
+        }
+        return res;
+    }
+    private Set<InsulatedCopperCableEntity> getConnectedCables(){
+        Set<InsulatedCopperCableEntity> res = new HashSet<>(6);
+        if(world.getBlockEntity(pos.up())instanceof InsulatedCopperCableEntity)
+            res.add((InsulatedCopperCableEntity) world.getBlockEntity(pos.up()));
+        if(world.getBlockEntity(pos.down())instanceof InsulatedCopperCableEntity)
+            res.add((InsulatedCopperCableEntity) world.getBlockEntity(pos.down()));
+        if(world.getBlockEntity(pos.north())instanceof InsulatedCopperCableEntity)
+            res.add((InsulatedCopperCableEntity) world.getBlockEntity(pos.north()));
+        if(world.getBlockEntity(pos.east())instanceof InsulatedCopperCableEntity)
+            res.add((InsulatedCopperCableEntity) world.getBlockEntity(pos.east()));
+        if(world.getBlockEntity(pos.south())instanceof InsulatedCopperCableEntity)
+            res.add((InsulatedCopperCableEntity) world.getBlockEntity(pos.south()));
+        if(world.getBlockEntity(pos.west())instanceof InsulatedCopperCableEntity)
+            res.add((InsulatedCopperCableEntity) world.getBlockEntity(pos.west()));
+        return res;
+    }
+    private static boolean updateStorages(World world, BlockPos pos, InsulatedCopperCableEntity entity){
+        Set<EnergyStorage> storages = entity.getSurroundingStorages(world,pos);
+        if(!entity.connectedStorages.equals(storages)){
+            entity.connectedStorages = storages;
+            return true;
+        }
+        return false;
+    }
+    private Set<EnergyStorage> getSurroundingStorages(World world, BlockPos pos){
+        Set<EnergyStorage> res = new HashSet<>();
+        for(Direction direction : Direction.values()){
+            EnergyStorage storage = EnergyStorage.SIDED.find(world,pos.add(direction.getVector()),direction);
+            if(storage!=null)
+                res.add(storage);
+        }
+        return res;
     }
 }
