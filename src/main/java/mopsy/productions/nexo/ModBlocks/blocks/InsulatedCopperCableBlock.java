d476e9b6dce172d2f82b638f2e746531cb57ea06
mopsy14 <Redstone.computerboy@gmail.com>
2023-09-27 12:51:08 +0200

    v13 Cables Added the correct voxelshapes to the insulated copper cables.

--- src/main/java/mopsy/productions/nexo/ModBlocks/blocks/InsulatedCopperCableBlock.java
+++ src/main/java/mopsy/productions/nexo/ModBlocks/blocks/InsulatedCopperCableBlock.java
@@ -15,6 +15,8 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.state.StateManager;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
@@ -31,6 +33,13 @@ import static net.minecraft.state.property.Properties.*;
 
 @SuppressWarnings("deprecation")
 public class InsulatedCopperCableBlock extends BlockWithEntity implements IModID, BlockEntityProvider {
+    private static final VoxelShape MID_SHAPE = VoxelShapes.cuboid(0.375, 0.375, 0.375, 0.625, 0.625, 0.625);
+    private static final VoxelShape UP_SHAPE = VoxelShapes.cuboid(0.375, 0.625, 0.375, 0.625, 1, 0.625);
+    private static final VoxelShape DOWN_SHAPE = VoxelShapes.cuboid(0.375, 0, 0.375, 0.625, 0.375, 0.625);
+    private static final VoxelShape NORTH_SHAPE = VoxelShapes.cuboid(0.375, 0.375, 0, 0.625, 0.625, 0.375);
+    private static final VoxelShape EAST_SHAPE = VoxelShapes.cuboid(0.625, 0.375, 0.375, 1, 0.625, 0.625);
+    private static final VoxelShape SOUTH_SHAPE = VoxelShapes.cuboid(0.375, 0.375, 0.625, 0.625, 0.625, 1);
+    private static final VoxelShape WEST_SHAPE = VoxelShapes.cuboid(0, 0.375, 0.375, 0.375, 0.625, 0.625);
     @Override
     public String getID(){return "insulated_copper_cable";}
 
@@ -43,6 +52,25 @@ public class InsulatedCopperCableBlock extends BlockWithEntity implements IModID
                         .nonOpaque()
         );
         this.setDefaultState(this.stateManager.getDefaultState().with(UP, false).with(DOWN, false).with(NORTH, false).with(EAST, false).with(SOUTH, false).with(WEST, false));
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        VoxelShape res = MID_SHAPE;
+        if(state.get(UP))
+            res = VoxelShapes.union(res, UP_SHAPE);
+        if(state.get(DOWN))
+            res = VoxelShapes.union(res, DOWN_SHAPE);
+        if(state.get(NORTH))
+            res = VoxelShapes.union(res, NORTH_SHAPE);
+        if(state.get(EAST))
+            res = VoxelShapes.union(res, EAST_SHAPE);
+        if(state.get(SOUTH))
+            res = VoxelShapes.union(res, SOUTH_SHAPE);
+        if(state.get(WEST))
+            res = VoxelShapes.union(res, WEST_SHAPE);
+
+        return res;
     }
 
     @Nullable
