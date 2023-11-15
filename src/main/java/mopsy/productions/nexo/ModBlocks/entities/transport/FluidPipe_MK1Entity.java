8c9e848942a1cea129a15a4df35eb60ccc169a7f
mopsy14 <Redstone.computerboy@gmail.com>
2023-11-15 20:45:11 +0100

    v9 Fluid Pipes Added some (incomplete) code for the fluid pipe transactions.

--- src/main/java/mopsy/productions/nexo/ModBlocks/entities/transport/FluidPipe_MK1Entity.java
+++ src/main/java/mopsy/productions/nexo/ModBlocks/entities/transport/FluidPipe_MK1Entity.java
@@ -2,9 +2,14 @@ package mopsy.productions.nexo.ModBlocks.entities.transport;
 
 import mopsy.productions.nexo.registry.ModdedBlockEntities;
 import mopsy.productions.nexo.registry.ModdedBlocks;
+import mopsy.productions.nexo.util.DualType;
 import mopsy.productions.nexo.util.NEXORotation;
 import mopsy.productions.nexo.util.PipeEndState;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
+import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.NbtCompound;
@@ -19,7 +24,8 @@ import java.util.*;
 @SuppressWarnings("UnstableApiUsage")
 public class FluidPipe_MK1Entity extends BlockEntity {
 
-    public Set<EnergyStorage> connectedStorages = new HashSet<>(6);
+    public Set<Storage<FluidVariant>> connectedOutputStorages = new HashSet<>(6);
+    public Set<Storage<FluidVariant>> connectedInputStorages = new HashSet<>(6);
     public Map<NEXORotation,PipeEndState> endStates = new HashMap<>();
     private boolean initializedStates = false;
 
@@ -68,39 +74,48 @@ public class FluidPipe_MK1Entity extends BlockEntity {
         if(world.isClient)return;
 
         updateStorages(world,blockPos,entity);
-/*
-        if(entity.energyStorage.amount > 0){
 
-            List<EnergyStorage> storages = new ArrayList<>(entity.getAllOutputStorages());
-            Collections.shuffle(storages);
+        try (Transaction transaction = Transaction.openOuter()) {
+            for (Storage<FluidVariant> inputStorage : entity.connectedInputStorages) {
+                for(StorageView<FluidVariant> inputStorageView : inputStorage){
+                    try (Transaction inputTransaction = transaction.openNested()) {
+                        long inputted = inputStorageView.extract(inputStorageView.getResource(), 405,inputTransaction);
+                        if (inputted > 0) {
 
-            long totalExchanged = 0;
-            while(!storages.isEmpty() && entity.energyStorage.amount>0){
-                try (Transaction transaction = Transaction.openOuter()) {
-                    totalExchanged += EnergyStorageUtil.move(
-                            entity.energyStorage,
-                            storages.get(storages.size()-1),
-                            POWER_RATE-totalExchanged,
-                            transaction
-                    );
-                    transaction.commit();
+                            List<EnergyStorage> storages = new ArrayList<>(entity.getAllOutputStorages());
+                            Collections.shuffle(storages);
+
+                            long totalExchanged = 0;
+                            while (!storages.isEmpty() && entity.energyStorage.amount > 0) {
+                                try (Transaction transaction = Transaction.openOuter()) {
+                                    totalExchanged += EnergyStorageUtil.move(
+                                            entity.energyStorage,
+                                            storages.get(storages.size() - 1),
+                                            POWER_RATE - totalExchanged,
+                                            transaction
+                                    );
+                                    transaction.commit();
+                                }
+                                storages.remove(storages.size() - 1);
+                            }
+                        }else inputTransaction.abort();
+                    }
                 }
-                storages.remove(storages.size()-1);
             }
         }
- */
+
     }
-    private Set<EnergyStorage> getAllOutputStorages(){
+    private Set<Storage<FluidVariant>> getAllOutputStorages(){
 
-        Set<EnergyStorage> res = new HashSet<>();
+        Set<Storage<FluidVariant>> res = new HashSet<>();
 
-        for(FluidPipe_MK1Entity cable : getAllConnectedCables()){
-            res.addAll(cable.connectedStorages);
+        for(FluidPipe_MK1Entity pipe : getAllConnectedPipes()){
+            res.addAll(pipe.connectedOutputStorages);
         }
 
         return res;
     }
-    private List<FluidPipe_MK1Entity> getAllConnectedCables(){
+    private List<FluidPipe_MK1Entity> getAllConnectedPipes(){
         List<FluidPipe_MK1Entity> res = new ArrayList<>();
         List<FluidPipe_MK1Entity> toCheck = new ArrayList<>();
         List<BlockPos> shouldIgnore = new ArrayList<>();
@@ -110,56 +125,55 @@ public class FluidPipe_MK1Entity extends BlockEntity {
         while(!toCheck.isEmpty()){
             int index = toCheck.size()-1;
             res.add(toCheck.get(index));
-            toCheck.addAll(getSurroundingCablePosses(toCheck.get(toCheck.size()-1).pos, shouldIgnore));
+            toCheck.addAll(getSurroundingPipePosses(toCheck.get(toCheck.size()-1).pos, shouldIgnore));
             toCheck.remove(index);
         }
 
         return res;
     }
-    private List<FluidPipe_MK1Entity> getSurroundingCablePosses(BlockPos pos, List<BlockPos> shouldIgnore){
+    private List<FluidPipe_MK1Entity> getSurroundingPipePosses(BlockPos pos, List<BlockPos> shouldIgnore){
         List<FluidPipe_MK1Entity> res = new ArrayList<>(6);
         for(Direction direction : Direction.values()){
              BlockPos iteratorPos = pos.offset(direction);
              if(!shouldIgnore.contains(iteratorPos))
-                if(world.getBlockEntity(iteratorPos) instanceof FluidPipe_MK1Entity cable) {
-                    res.add(cable);
+                if(world.getBlockEntity(iteratorPos) instanceof FluidPipe_MK1Entity pipe) {
+                    res.add(pipe);
                     shouldIgnore.add(iteratorPos);
                 }
         }
         return res;
     }
-    private Set<FluidPipe_MK1Entity> getConnectedCables(){
-        Set<FluidPipe_MK1Entity> res = new HashSet<>(6);
-        if(world.getBlockEntity(pos.up())instanceof FluidPipe_MK1Entity)
-            res.add((FluidPipe_MK1Entity) world.getBlockEntity(pos.up()));
-        if(world.getBlockEntity(pos.down())instanceof FluidPipe_MK1Entity)
-            res.add((FluidPipe_MK1Entity) world.getBlockEntity(pos.down()));
-        if(world.getBlockEntity(pos.north())instanceof FluidPipe_MK1Entity)
-            res.add((FluidPipe_MK1Entity) world.getBlockEntity(pos.north()));
-        if(world.getBlockEntity(pos.east())instanceof FluidPipe_MK1Entity)
-            res.add((FluidPipe_MK1Entity) world.getBlockEntity(pos.east()));
-        if(world.getBlockEntity(pos.south())instanceof FluidPipe_MK1Entity)
-            res.add((FluidPipe_MK1Entity) world.getBlockEntity(pos.south()));
-        if(world.getBlockEntity(pos.west())instanceof FluidPipe_MK1Entity)
-            res.add((FluidPipe_MK1Entity) world.getBlockEntity(pos.west()));
-        return res;
-    }
     private static boolean updateStorages(World world, BlockPos pos, FluidPipe_MK1Entity entity){
-        Set<EnergyStorage> storages = entity.getSurroundingStorages(world,pos);
-        if(!entity.connectedStorages.equals(storages)){
-            entity.connectedStorages = storages;
-            return true;
+        Set<DualType<NEXORotation,Storage<FluidVariant>>> storages = entity.getSurroundingStorages(world,pos);
+        Set<Storage<FluidVariant>> inputStorages = new HashSet<>(6);
+        Set<Storage<FluidVariant>> outputStorages = new HashSet<>(6);
+        for (DualType<NEXORotation,Storage<FluidVariant>> dualType : storages) {
+            if(entity.endStates.get(dualType.var1).isInput()){
+                inputStorages.add(dualType.var2);
+            }
+            if(entity.endStates.get(dualType.var1).isOutput()){
+                outputStorages.add(dualType.var2);
+            }
         }
-        return false;
+        boolean changed = false;
+        if(!entity.connectedInputStorages.equals(inputStorages)) {
+            entity.connectedInputStorages = inputStorages;
+            changed = true;
+        }
+        if(!entity.connectedOutputStorages.equals(outputStorages)) {
+            entity.connectedOutputStorages = outputStorages;
+            changed = true;
+        }
+        return changed;
     }
-    private Set<EnergyStorage> getSurroundingStorages(World world, BlockPos pos){
-        Set<EnergyStorage> res = new HashSet<>();
-        for(Direction direction : Direction.values()){
-            BlockPos calculatedPos = pos.offset(direction);
+    private Set<DualType<NEXORotation,Storage<FluidVariant>>> getSurroundingStorages(World world, BlockPos pos){
+        Set<DualType<NEXORotation,Storage<FluidVariant>>> res = new HashSet<>();
+        for(NEXORotation rotation : NEXORotation.values()){
+            BlockPos calculatedPos = pos.offset(rotation.direction);
             if(!(world.getBlockEntity(calculatedPos) instanceof FluidPipe_MK1Entity)) {
-                EnergyStorage storage = EnergyStorage.SIDED.find(world, calculatedPos, direction);
+                Storage<FluidVariant> storage = FluidStorage.SIDED.find(world, calculatedPos, rotation.direction);
                 if (storage != null)
-                    res.add(storage);
+                    res.add(new DualType<>(rotation,storage));
             }
         }
         return res;
--- src/main/java/mopsy/productions/nexo/util/PipeEndState.java
+++ src/main/java/mopsy/productions/nexo/util/PipeEndState.java
@@ -16,4 +16,6 @@ public enum PipeEndState {
     public boolean isNone(){
         return this == NONE;
     }
+    public boolean isInput(){return this == IN || this == IN_OUT;}
+    public boolean isOutput(){return this == OUT || this == IN_OUT;}
 }
