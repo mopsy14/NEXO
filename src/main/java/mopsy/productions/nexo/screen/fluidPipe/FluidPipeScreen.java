f75ca75c2182965dc30d85842a2732409735243e
mopsy14 <Redstone.computerboy@gmail.com>
2023-12-03 21:23:44 +0100

    v21 Fluid Pipes Finished gui code of the fluid pipes (maybe add colors later). Only click behavior still needs to be added.

--- src/main/java/mopsy/productions/nexo/screen/fluidPipe/FluidPipeScreen.java
+++ src/main/java/mopsy/productions/nexo/screen/fluidPipe/FluidPipeScreen.java
@@ -1,12 +1,9 @@
 package mopsy.productions.nexo.screen.fluidPipe;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import mopsy.productions.nexo.ModBlocks.entities.machines.CrusherEntity;
 import mopsy.productions.nexo.ModBlocks.entities.transport.FluidPipe_MK1Entity;
-import mopsy.productions.nexo.interfaces.IEnergyStorage;
-import mopsy.productions.nexo.util.DisplayUtils;
 import mopsy.productions.nexo.util.IntCords2D;
-import net.minecraft.block.entity.BlockEntity;
+import mopsy.productions.nexo.util.NEXORotation;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
@@ -20,9 +17,16 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import static mopsy.productions.nexo.Main.modid;
+import static mopsy.productions.nexo.util.NEXORotation.*;
 
 public class FluidPipeScreen extends HandledScreen<FluidPipeScreenHandler> {
     private static final Identifier TEXTURE = new Identifier(modid, "textures/gui/fluid_pipe.png");
+    public Predicate<IntCords2D> renderUpTooltipPredicate;
+    public Predicate<IntCords2D> renderDownTooltipPredicate;
+    public Predicate<IntCords2D> renderNorthTooltipPredicate;
+    public Predicate<IntCords2D> renderEastTooltipPredicate;
+    public Predicate<IntCords2D> renderSouthTooltipPredicate;
+    public Predicate<IntCords2D> renderWestTooltipPredicate;
     public Predicate<IntCords2D> renderEnergyTooltipPredicate;
 
     public FluidPipeScreen(FluidPipeScreenHandler handler, PlayerInventory inventory, Text title) {
@@ -44,24 +48,57 @@ public class FluidPipeScreen extends HandledScreen<FluidPipeScreenHandler> {
         int y = (height - backgroundHeight)/2;
         drawTexture(matrices, x, y, 0, 0, backgroundWidth, backgroundHeight);
 
-        drawSlots(matrices, x, y);
-
-        //renderEnergyTooltipPredicate = ScreenUtils.renderEnergyStorage(this, matrices, x+147, y+11, getPower(), CrusherEntity.POWER_CAPACITY);
-    }
-
-    private void drawSlots(MatrixStack matrices, int x, int y){
 
         if(MinecraftClient.getInstance().world.getBlockEntity(handler.getBlockPos()) instanceof FluidPipe_MK1Entity entity) {
-            itemRenderer.renderInGui(entity.getWorld().getBlockState(entity.getPos().add(0,0,1)).getBlock().asItem().getDefaultStack(),x+50,y+50);
+            renderUpTooltipPredicate = renderSlot(matrices, x+19, y+29, TEXTURE, entity, UP);
+            renderDownTooltipPredicate = renderSlot(matrices, x+19, y+69, TEXTURE, entity, DOWN);
+            renderNorthTooltipPredicate = renderSlot(matrices, x+69, y+29, TEXTURE, entity, NORTH);
+            renderEastTooltipPredicate = renderSlot(matrices, x+89, y+49, TEXTURE, entity, EAST);
+            renderSouthTooltipPredicate = renderSlot(matrices, x+69, y+69, TEXTURE, entity, SOUTH);
+            renderWestTooltipPredicate = renderSlot(matrices, x+49, y+49, TEXTURE, entity, WEST);
         }
+    }
+
+    public Predicate<IntCords2D> renderSlot(MatrixStack matrices, int x, int y, Identifier texture, FluidPipe_MK1Entity entity, NEXORotation rotation){
+
+        if(entity.endStates.get(rotation).isInput()) {
+            RenderSystem.setShaderTexture(0, texture);
+            drawTexture(matrices, x, y, 176, 0, 20, 20);
+            itemRenderer.renderInGui(entity.getWorld().getBlockState(entity.getPos().add(rotation.transformToVec3i())).getBlock().asItem().getDefaultStack(),x+2,y+2);
+        }else if(entity.endStates.get(rotation).isOutput()){
+            RenderSystem.setShaderTexture(0, texture);
+            drawTexture(matrices, x, y, 196, 0, 20, 20);
+            itemRenderer.renderInGui(entity.getWorld().getBlockState(entity.getPos().add(rotation.transformToVec3i())).getBlock().asItem().getDefaultStack(),x+2,y+2);
+        }else{
+            RenderSystem.setShaderTexture(0, texture);
+            drawTexture(matrices, x, y, 216, 0, 20, 20);
+            itemRenderer.renderInGui(entity.getWorld().getBlockState(entity.getPos().add(rotation.transformToVec3i())).getBlock().asItem().getDefaultStack(),x+2,y+2);
+        }
+
+        return ic2d -> (
+                ic2d.x>x+2 && ic2d.x<x+18 &&
+                        ic2d.y>y+2 && ic2d.y<y+18
+        );
     }
 
     @Override
     protected void drawMouseoverTooltip(MatrixStack matrices, int x, int y) {
         super.drawMouseoverTooltip(matrices, x, y);
-        IntCords2D mouse = new IntCords2D(x,y);
-        if (renderEnergyTooltipPredicate.test(mouse))
-            renderEnergyTooltip(hasShiftDown(), matrices, mouse);
+        if(MinecraftClient.getInstance().world.getBlockEntity(handler.getBlockPos()) instanceof FluidPipe_MK1Entity entity) {
+            IntCords2D mouse = new IntCords2D(x, y);
+            if (renderUpTooltipPredicate.test(mouse))
+                renderSlotTooltip(matrices, mouse, entity, UP);
+            if (renderDownTooltipPredicate.test(mouse))
+                renderSlotTooltip(matrices, mouse, entity, DOWN);
+            if (renderNorthTooltipPredicate.test(mouse))
+                renderSlotTooltip(matrices, mouse, entity, NORTH);
+            if (renderEastTooltipPredicate.test(mouse))
+                renderSlotTooltip(matrices, mouse, entity, EAST);
+            if (renderSouthTooltipPredicate.test(mouse))
+                renderSlotTooltip(matrices, mouse, entity, SOUTH);
+            if (renderWestTooltipPredicate.test(mouse))
+                renderSlotTooltip(matrices, mouse, entity, WEST);
+        }
     }
 
     @Override
@@ -69,11 +106,18 @@ public class FluidPipeScreen extends HandledScreen<FluidPipeScreenHandler> {
         this.textRenderer.draw(matrices, this.title, (float)this.titleX, (float)this.titleY, 4210752);
     }
 
-    private void renderEnergyTooltip(boolean exact, MatrixStack matrices, IntCords2D mouseCords){
+    private void renderSlotTooltip(MatrixStack matrices, IntCords2D mouseCords, FluidPipe_MK1Entity entity, NEXORotation rotation){
         List<Text> text = new ArrayList<>();
-        text.add(Text.of(DisplayUtils.getEnergyBarText(getPower(), CrusherEntity.POWER_CAPACITY, hasShiftDown())));
-        if(!exact)
-            text.add(Text.of("Hold shift for advanced view"));
+        text.add(Text.of("Side: "+rotation.id.toUpperCase()));
+        text.add(Text.of("Block name:"));
+        text.add(Text.translatable(entity.getWorld().getBlockState(entity.getPos().add(rotation.transformToVec3i())).getBlock().getTranslationKey()));
+        text.add(Text.of("Current mode: "+entity.endStates.get(rotation).toString()));
+        if(entity.endStates.get(rotation).isEnd())
+            text.add(Text.of("Click to switch input and output"));
+        if(entity.endStates.get(rotation).isPipe())
+            text.add(Text.of("Another pipe is connected here"));
+        if(entity.endStates.get(rotation).isNone())
+            text.add(Text.of("No fluid storage was found on this side"));
         renderTooltip(matrices, text, mouseCords.x, mouseCords.y);
     }
 
@@ -81,14 +125,6 @@ public class FluidPipeScreen extends HandledScreen<FluidPipeScreenHandler> {
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         renderBackground(matrices);
         super.render(matrices, mouseX, mouseY, delta);
-        //drawMouseoverTooltip(matrices, mouseX, mouseY);
-    }
-
-    private long getPower(){
-        BlockEntity blockEntity = MinecraftClient.getInstance().world.getBlockEntity(handler.getBlockPos());
-        if(blockEntity instanceof IEnergyStorage) {
-            return ((IEnergyStorage) blockEntity).getPower();
-        }
-        return 0;
+        drawMouseoverTooltip(matrices, mouseX, mouseY);
     }
 }
