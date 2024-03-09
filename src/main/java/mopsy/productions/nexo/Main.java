4518babcf4535268f278335bb58781cab6903224
Collin <collinvanmierlo2008@gmail.com>
2024-03-09 11:25:47 +0100

    v0.0.505 Changed the 2nd items tab to tools tab

--- src/main/java/mopsy/productions/nexo/Main.java
+++ src/main/java/mopsy/productions/nexo/Main.java
@@ -23,7 +23,7 @@ public class Main implements ModInitializer {
 	public static final ItemGroup CREATIVE_TAB = FabricItemGroupBuilder.build(
 			new Identifier(modid, "nexo_items"), () -> new ItemStack(ModdedItems.Items.get("uranium_ingot")));
 	public static final ItemGroup CREATIVE_TOOLS_TAB = FabricItemGroupBuilder.build(
-			new Identifier(modid, "nexo_items"), () -> new ItemStack(ModdedItems.Items.get("titanium_pickaxe")));
+			new Identifier(modid, "nexo_tools"), () -> new ItemStack(ModdedItems.Items.get("titanium_pickaxe")));
 	public static final ItemGroup CREATIVE_BLOCK_TAB = FabricItemGroupBuilder.build(
 			new Identifier(modid, "nexo_blocks"), () -> new ItemStack(ModdedBlocks.BlockItems.get("uranium_block")));
 	public static final ItemGroup CREATIVE_FLUIDS_TAB = FabricItemGroupBuilder.build(
