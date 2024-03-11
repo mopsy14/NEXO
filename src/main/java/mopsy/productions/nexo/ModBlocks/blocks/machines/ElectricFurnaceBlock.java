8d98a1bd2b72f32f3de5d7c04c080f13335d47f6
mopsy14 <Redstone.computerboy@gmail.com>
2024-03-11 16:53:16 +0100

    v0.0.516 Started adding some classes for the electric furnace.

---
+++ src/main/java/mopsy/productions/nexo/ModBlocks/blocks/machines/ElectricFurnaceBlock.java
@@ -0,0 +1,95 @@
+package mopsy.productions.nexo.ModBlocks.blocks.machines;
+
+import mopsy.productions.nexo.ModBlocks.entities.machines.PressEntity;
+import mopsy.productions.nexo.interfaces.IModID;
+import mopsy.productions.nexo.registry.ModdedBlockEntities;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.*;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class ElectricFurnaceBlock extends BlockWithEntity implements IModID, BlockEntityProvider {
+    public static final DirectionProperty FACING;
+    @Override
+    public String getID(){return "electric_furnace";}
+
+    public ElectricFurnaceBlock() {
+        super(FabricBlockSettings
+                .of(Material.METAL, MapColor.BLACK)
+                .strength(4.0F, 8.0F)
+                .sounds(BlockSoundGroup.METAL)
+                .requiresTool()
+        );
+        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
+    }
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+    }
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if(!world.isClient){
+            NamedScreenHandlerFactory screenHandlerFactory = (PressEntity)world.getBlockEntity(pos);
+            if(screenHandlerFactory != null){
+                player.openHandledScreen(screenHandlerFactory);
+            }
+        }
+        return ActionResult.SUCCESS;
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new PressEntity(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        return checkType(type, ModdedBlockEntities.PRESS, PressEntity::tick);
+    }
+
+    @Override
+    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof PressEntity) {
+            ItemScatterer.spawn(world, pos, (PressEntity) blockEntity);
+            world.updateComparators(pos, this);
+        }
+        super.onBreak(world, pos, state, player);
+    }
+
+    static {
+        FACING = HorizontalFacingBlock.FACING;
+    }
+
+}
